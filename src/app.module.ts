b9551179f872f65cc7254addbe3caecebe1de6da
Hamza AbdulHameed <hamzaemad598@gmail.com>
2024-06-22 01:14:03 +0300

    [add] orders module and cart module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OrdersModule } from './orders/orders.module';
+import { CartModule } from './cart/cart.module';
 
 @Module({
-  imports: [],
+  imports: [OrdersModule, CartModule],
   controllers: [AppController],
   providers: [AppService],
 })
