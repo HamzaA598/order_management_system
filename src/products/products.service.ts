717cd23856fa4ed444745ae1902d23f30f1e7984
Hamza AbdulHameed <hamzaemad598@gmail.com>
2024-06-22 16:16:08 +0300

    [add] products module

---
+++ src/products/products.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { PrismaService } from '../prisma.service';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+
+@Injectable()
+export class ProductsService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(createProductDto: CreateProductDto) {
+    const { name, description, price, stock } = createProductDto;
+
+    const newProduct = await this.prisma.product.create({
+      data: {
+        name,
+        description,
+        price,
+        stock,
+      },
+    });
+
+    return newProduct;
+  }
+
+  findAll() {
+    return `This action returns all products`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} product`;
+  }
+
+  update(id: number, updateProductDto: UpdateProductDto) {
+    return `This action updates a #${id} product`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} product`;
+  }
+}
