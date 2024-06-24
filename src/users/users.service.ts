11983f5b9380a995da604a2e6a4b5fa5580db40a
Hamza AbdulHameed <hamzaemad598@gmail.com>
2024-06-24 03:05:15 +0300

    [add] delete user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 import { PrismaService } from '../prisma.service';
-import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class UsersService {
@@ -23,19 +21,9 @@ export class UsersService {
     return newUser;
   }
 
-  findAll() {
-    return `This action returns all users`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async remove(userId: number) {
+    return await this.prisma.user.delete({
+      where: { userId: userId },
+    });
   }
 }
